6e2419b6fce209049ad7f534deea7f4ba855762a
dell <1754712311@qq.com>
2020-02-24 18:56:36 +0800

    jakeLi02

---
+++ repo/jcart-administratio-back/src/test/java/io/ljh/jcartadministratioback/JcartAdministratioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ljh.jcartadministratioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartAdministratioBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
